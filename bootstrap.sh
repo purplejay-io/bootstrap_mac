8ae8d7b93235f71cb10bdb9c5769eed7b48bf0c0
Brian Carlin <brian@purplejay.io>
2020-04-15 18:46:57 -0700

    missed a return

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,5 @@ curl -L aka.ms/EnrollMyMac --output /tmp/intune.pkg
 
 softwareupdate --fetch-full-installer
 
-'/Applications/Install macOS Catalina.app/Contents/Resources/startosinstall' --eraseinstall --agreetolicense --forcequitapps --newvolumename 'Macintosh HD' --installpackage '/tmp/intune.pkg'
+'/Applications/Install macOS Catalina.app/Contents/Resources/startosinstall' --eraseinstall --agreetolicense \
+--forcequitapps --newvolumename 'Macintosh HD' --installpackage '/tmp/intune.pkg'
