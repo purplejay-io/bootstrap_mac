8786665576d8d24c2b51933fce08707e789831ae
Brian Carlin <brian@purplejay.io>
2021-03-07 13:31:22 -0800

    Update and rename bootstrap.sh to restore.sh

--- bootstrap.sh
+++
@@ -1,8 +0,0 @@
-# https://www.jamf.com/blog/reinstall-a-clean-macos-with-one-button/
-
-curl -L aka.ms/EnrollMyMac --output /tmp/intune.pkg
-
-softwareupdate --fetch-full-installer
-
-'/Applications/Install macOS Catalina.app/Contents/Resources/startosinstall' --eraseinstall --agreetolicense \
---forcequitapps --newvolumename 'Macintosh HD' --installpackage '/tmp/intune.pkg'
---
+++ restore.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# https://www.jamf.com/blog/reinstall-a-clean-macos-with-one-button/
+
+#curl -L aka.ms/EnrollMyMac --output /tmp/intune.pkg
+
+softwareupdate --fetch-full-installer
+
+'/Applications/Install macOS Big Sur.app/Contents/Resources/startosinstall' --eraseinstall --agreetolicense \
+--forcequitapps --newvolumename 'Macintosh HD' --passprompt
