dba8c337e8ecc9bbd0314a1b45d9d1086b6eed9d
Brian Carlin <brian@purplejay.io>
2021-03-07 17:00:39 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -36,15 +36,17 @@ defaults write com.apple.finder ShowStatusBar -int 1
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+if [[ `uname -m` == 'arm64' ]]; then
+    sudo softwareupdate --install-rosetta
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
+
 brew install python3 npm nmap tree wget 
 
-brew install --cask github
-brew install --cask visual-studio-code
-brwe install --cask intune-company-portal
-brew install --cask microsoft-edge
-brew install --cask microsoft-office
-brew install --cask 1password
-brew install --cask homebrew/cask-drivers/yubico-yubikey-manager
+brew install --cask github visual-studio-code
+brwe install --cask intune-company-portal microsoft-edge microsoft-office microsoft-teams
+brew install --cask 1password homebrew/cask-drivers/yubico-yubikey-manager
 brew install --cask jetbrains-toolbox
 brew install --cask firefox
 brew install --cask mactex
